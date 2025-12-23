d1eab9505916077529cd0fbe54327e98e1280a4f
ChoiMinSang <min393917@gmail.com>
2025-12-23 10:43:39 +0900

    day02 - memory management

--- Day2/app01.cpp
+++ Day2/app01.cpp
@@ -2,6 +2,11 @@
 using namespace std; 
 
 int main() {
+	int kid = 3000;
+	int adult = 10000;
+	int senior = 5000;
+	int total = 0;
+
 	int human = 0;
 	// int ages[human]
 
@@ -10,6 +15,26 @@ int main() {
 
 	// heap 메모리 할당
 	int* ages = new int[human]; 
+
+	for (int i = 0; i < human; i++) {
+		cout << "나이 입력: ";
+		cin >> ages[i];
+	}
+
+	for (int i = 0; i < human; i++) {
+		int age = *(ages + i);
+
+		if (age < 9) {
+			total = total + kid;
+		}
+		else if (age < 65) {
+			total = total + adult;
+		}
+		else {
+			total = total + senior;
+		}
+	}
+
 
 	/*cout << ages;
 	cout << '\n';
@@ -20,8 +45,9 @@ int main() {
 	cout << &test;*/
 
 
-	// heap 메모리 해제
-	delete[] ages;
+	cout << human << "분의 총 요금은 " << total << "원 입니다\n";
+	delete[] ages;  // free heap memory
+	ages = nullptr;  // clear memory address
 
 	return 0;
 }
