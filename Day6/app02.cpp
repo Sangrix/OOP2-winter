bd540fe9eb778c63e12036f4f04bec3357550594
ChoiMinSang <min393917@gmail.com>
2025-12-30 10:57:55 +0900

    day06 - Heap memory Pokemon & virtual Destructors

--- Day6/app02.cpp
+++ Day6/app02.cpp
@@ -13,11 +13,11 @@ class Pokemon {
     public:
         Pokemon(const char* _name, int _level, int _hp) : level(_level), hp(_hp) {
             strcpy(name, _name);
-            cout << name << "가(이) 나타났다!" << endl;
+            cout << name << "가(이) 나타났다!(부모 클래스)" << endl;
         }
 
-        ~Pokemon() {
-            cout << name << "는(은) 사라졌다..." << endl;
+        virtual ~Pokemon() {
+            cout << name << "는(은) 사라졌다...(부모 클래스)" << endl;
         }
 
        void info() const {
@@ -42,6 +42,11 @@ class Pokemon {
 class Charizard : public Pokemon {
     public:
         Charizard(const char* _name, int _level, int _hp): Pokemon(_name, _level, _hp) {
+            cout << name << "가(이) 나타났다!(자식 클래스)" << endl;
+        }
+        
+        ~Charizard() {
+            cout << name << "는(은) 사라졌다...(자식 클래스)" << endl;
         }
 
     void info() const {
@@ -58,6 +63,11 @@ class Charizard : public Pokemon {
 class Pikachu : public Pokemon {
     public:
         Pikachu(const char* _name, int _level, int _hp) : Pokemon(_name, _level, _hp) {
+            cout << name << "가(이) 나타났다!(자식 클래스)" << endl;
+        }
+
+        ~Pikachu() {
+            cout << name << "는(은) 사라졌다...(자식 클래스)" << endl;
         }
 
         void info() const {
@@ -77,7 +87,13 @@ class Pikachu : public Pokemon {
 class Squirtle : public Pokemon {
     public:
         Squirtle(const char* _name, int _level, int _hp) : Pokemon(_name, _level, _hp) {
+            cout << name << "가(이) 나타났다!(자식 클래스)" << endl;
         }
+
+        ~Squirtle() {
+            cout << name << "는(은) 사라졌다...(자식 클래스)" << endl;
+        }
+
 
         void info() const {
             Pokemon::info();
@@ -92,23 +108,32 @@ class Squirtle : public Pokemon {
 
 int main() {
     Charizard c("리자몽", 15, 60);
-    Pikachu p("피카츄", 10, 35);
+    Pikachu p1("피카츄", 10, 35);
     Squirtle s("꼬부기", 12, 45);
+
+    Pokemon* p2 = new Pikachu("야생의 피카츄", 20, 100);
 
     c.info();
     cout << endl;
 
-    p.info();
+    p1.info();
     cout << endl;
     
     s.info();
     cout << endl;
 
+    p2->info();
+    cout << endl;
+
+    // 부모 클래스의 포인터 변수로 자식 객체를 지우려고 시도
+    // 부모 클래스의 소멸자에 virtual이 없으면 부모 소멸자만 돌아가서 memory leak
+    delete p2;
+
     // 리스코프 치환원칙으로 포인터 배열 선언
     // 포인터는 주소(stack, heap)만 받을 수 있음
     Pokemon* team[3];
     team[0] = &c;
-    team[1] = &p;
+    team[1] = &p1;
     team[2] = &s;
 
     for (int i = 0; i < 3; ++i) {
@@ -117,7 +142,7 @@ int main() {
         cout << endl;
     }
 
-    p.pikaVoice();
+    p1.pikaVoice();
 
     cout << endl << "프로그램 종료" << endl;
     return 0;
