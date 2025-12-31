8f790dce143abe1858740d0764198134625e06ca
ChoiMinSang <min393917@gmail.com>
2025-12-31 11:35:22 +0900

    day 07 - operator overloading

--- Day7/app02.cpp
+++ Day7/app02.cpp
@@ -12,13 +12,32 @@ public:
 	}
 
 	//// 연산자의 왼쪽은 실행 타임의 객체 주소로 관리, 오른쪽은 right 매개변수
-	//Complex operator+(Complex right) {
+	//Complex operator+(const Complex& right) {
 	//	return Complex(this->realNumber + right.realNumber, this->imaginary + right.imaginary);
 	//}
+
+	Complex operator++() {
+		this->realNumber = this->realNumber + 1;
+
+		return Complex(this->realNumber, this->imaginary);
+	}
+
+	/*Complex& operator++() {
+		this->realNumber = this->realNumber + 1;
+
+		return *this;
+	}*/
+
+	Complex operator++(int) {
+		Complex previous(this->realNumber, this->imaginary);
+
+		this->realNumber = this->realNumber + 1;
+		return previous;
+	}
 };
 
 // 일반 함수 (멤버함수가 아닌 경우)
-Complex operator+(Complex left, Complex right) {
+Complex operator+(const Complex& left, const Complex& right) {
 	return Complex(left.realNumber + right.realNumber, left.imaginary + right.imaginary);
 }
 
@@ -26,10 +45,17 @@ int main() {
 	Complex c1(9, 7);
 	Complex c2(1, 4);
 	Complex c3 = c1 + c2;
-	Complex c3 = operator+(c1, c2);	
+	// Complex c3 = operator+(c1, c2);	
 	// Complex c3 = c1.operator+(c2);
 
 	cout << c3.realNumber << "+" << c3.imaginary << "i\n";
+	// ++c3;
+	// Complex c4 = ++c3;
+	
+	// 후위 연산이라 c3의 값을 먼저 던지고 증가되어야함
+	Complex c4 = c3++;
+	cout << c3.realNumber << "+" << c3.imaginary << "i\n";
+	cout << c4.realNumber << "+" << c4.imaginary << "i\n";
 
 	return 0;
 }
