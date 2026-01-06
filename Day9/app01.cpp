84d99e2aa9037075ef12a7e40faa229fc11e637e
ChoiMinSang <min393917@gmail.com>
2026-01-06 10:56:13 +0900

    day09 - dynamicArray class

---
+++ Day9/app01.cpp
@@ -0,0 +1,9 @@
+#include "dynamicarray.h"
+int main() {
+	DynamicArray da1(5);
+
+	da1.setAt(100, 3);
+	cout << da1.getAt(3) << '\n';
+
+	return 0;
+}
---
+++ Day9/dynamicarray.cpp
@@ -0,0 +1,32 @@
+#include "dynamicarray.h"
+using namespace std;
+
+DynamicArray::DynamicArray()
+{
+}
+
+DynamicArray::DynamicArray(int size)
+{
+	cout << "동적배열 객체를 생성. 힙메모리 할당\n";
+	ptr = new int[size];
+}
+
+DynamicArray::~DynamicArray()
+{
+	cout << "동적배열 객체 소멸. 메모리를 정리합니다.\n";
+	delete ptr;
+	ptr = nullptr;
+}
+
+void DynamicArray::setAt(int value, int index)
+{
+	cout << "힙 메모리 " << index << "번 위치의 값 " << value << "할당\n";
+	ptr[index] = value;
+}
+
+int DynamicArray::getAt(int index)
+{
+	return ptr[index];
+}
+
+
