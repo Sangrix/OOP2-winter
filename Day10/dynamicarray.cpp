a82cad957404e5faf492d2079267103257f36495
ChoiMinSang <min393917@gmail.com>
2026-01-07 09:24:09 +0900

    day10 - exception handling Constructor

---
+++ Day10/dynamicarray.cpp
@@ -0,0 +1,53 @@
+#include "dynamicarray.h"
+#include "myexception.h"
+using namespace std;
+
+DynamicArray::DynamicArray()
+{
+}
+
+DynamicArray::DynamicArray(int size):size(size)
+{
+	try {
+		//this->size = size;
+		cout << this << " 동적배열 객체를 생성. 힙메모리 할당\n";
+		ptr = new int[size];
+		throw MyException(1000, "메모리 오류\n", this);
+		//cout << this << " 동적배열 객체를 생성. 힙메모리 할당\n";
+	}
+	catch (...) {
+		delete[] ptr;
+		ptr = nullptr;
+	}
+}
+
+DynamicArray::~DynamicArray()
+{
+	cout << "동적배열 객체 소멸. 메모리를 정리합니다.\n";
+	delete[] ptr;
+	ptr = nullptr;
+}
+
+void DynamicArray::setAt(int value, int index)
+{
+	if ((index < 0) || (index >= size)) {
+		// throw 4885; // 정수를 throw
+		//throw "인덱스의 범위를 벗어나 값을 할당할 수 없습니다. 4885\n";
+		throw MyException(4885, "인덱스의 범위를 벗어나 값을 할당할 수 없습니다.", this);
+	}
+	cout << "힙 메모리 " << index << "번 위치의 값 " << value << "할당\n";
+	ptr[index] = value;
+}
+
+int DynamicArray::getAt(int index)
+{
+	if ((index < 0) || (index >= size)) {
+		//throw 4886; // 정수를 throw
+		//throw "인덱스의 범위를 벗어나 값을 꺼낼 수 없습니다. 4886\n";
+		throw MyException(4886, "인덱스의 범위를 벗어나 값을 꺼낼 수 없습니다.", this);
+	}
+
+	return ptr[index];
+}
+
+
