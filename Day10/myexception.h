3a81258d1e9773057de54ae6a89167b7e66a08ab
ChoiMinSang <min393917@gmail.com>
2026-01-07 09:32:24 +0900

    day10 - exception handling Constructor

---
+++ Day10/myexception.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "dynamicarray.h"
+
+class MyException {
+public:
+	int errorCode;
+	const char* errorDescription;
+	DynamicArray* errorAddress;
+
+	MyException(int errorCode, const char* errorDescription, DynamicArray* errorAddress);
+};
