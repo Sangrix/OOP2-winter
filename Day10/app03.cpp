cdc6429205acee7dfe8074067b9ce6d240f88fdb
ChoiMinSang <min393917@gmail.com>
2026-01-07 11:44:43 +0900

    day10 - template

--- Day10/app03.cpp
+++ Day10/app03.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include <cstring>
 using namespace std;
 
 template <typename T>
@@ -8,6 +9,15 @@ T smaller(T fst, T scd) {
 		return fst;
 	}
 	return scd;
+}
+
+template <>
+const char* smaller(const char* first, const const char* second) {
+	if (strcmp(first, second) < 0) {
+		// -1이면 두 문자열은 다르고, first가 더 작은 값
+		return first;
+	}
+	return second;
 }
 
 int main() {
@@ -20,6 +30,10 @@ int main() {
 	cout << "12.1 and 13 비교: " << smaller<double>(12.1, 13) << endl;
 	cout << "41.2 and 33.1 비교: " << smaller(41.2, 33.1) << endl;
 	cout << smaller("Hello", "Bi") << endl;
+
+	const char* s1 = "Bye";
+	const char* s2 = "Bye Bye";
+	cout << "s1과 s2 비교 " << smaller(s1, s2) << endl;
 
 	return 0;
 }
