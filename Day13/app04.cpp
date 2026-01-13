5ef7fc7698aecf491cd4d1b958ef1e4a143cd9db
ChoiMinSang <min393917@gmail.com>
2026-01-13 11:18:42 +0900

    day13 - for_each

---
+++ Day13/app04.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> vec1;
+vector<int> vec2;
+void print(int value)
+{
+	cout << value << "  ";
+}
+void square(int value)
+{
+	vec2.push_back(value * value);
+}
+int main()
+{
+	vec1.push_back(1);
+	vec1.push_back(2);
+	vec1.push_back(3);
+
+	// algorithm에 존재, 3번째 인자에 함수명
+	//for_each(vec.begin(), vec.end(), print);
+	for_each(vec1.begin(), vec1.end(), print);
+	cout << '\n';
+	for_each(vec1.begin(), vec1.end(), square);
+	cout << '\n';
+	for_each(vec2.begin(), vec2.end(), &print);
+	return 0;
+}
